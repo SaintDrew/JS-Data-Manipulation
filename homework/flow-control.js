ca7bbfb37f40cfa31c702fc692755185ba7fbc4b
SaintDrew <drew_el12@yahoo.com>
2024-04-14 19:27:07 -0400

    week 1

--- homework/flow-control.js
+++ homework/flow-control.js
@@ -10,13 +10,34 @@ const spaceReq = 0.8;
 const plants = 20;
 
 // Plant Growth
-const week1 = plants;
-const week2 = plants * 2;
-const week3 = week2 * 2;
+// const week1 = plants;
+// const week2 = plants * 2;
+// const week3 = week2 * 2; 
 
 // Control Flow 
 let space1 = spaceReq * plants;
 let space2 = spaceReq * week2;
 let space3 = spaceReq * week3;
 
-console.log(space3)
+// Part 1 answers // 
+let totalArea = 3.1415 * 5 * 5
+console.log(totalArea) // 78.53
+let TotalCapacity = totalArea/0.8
+console.log(TotalCapacity)// 98.17
+
+let week = 1
+if (growth = 20 * 2 * week)
+{
+    console.log(`${growth} : Week 1 Growth`)}
+
+    if ( growth > TotalCapacity * 0.8){
+        console.log("Pruned : Week 1")
+    } else if (growth > TotalCapacity * 0.5){
+        console.log("Monitored : Week 1")
+    } else {console.log("Planted : Week 1")}
+
+    let week2 = 2
+    if (growth2 = 20 * 2 * week2)
+    {
+        console.log(${growth2} : Week 2 Growth)}
+    
