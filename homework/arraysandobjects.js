b439ed00eb0f5e4760b38910d9c8581ba939c4f1
SaintDrew <drew_el12@yahoo.com>
2024-04-17 23:23:59 -0400

    Part 2

--- homework/arraysandobjects.js
+++ homework/arraysandobjects.js
@@ -4,12 +4,21 @@ console.log("Ready to TakeOff!!!")
 // Part 1: Refactoring Old Code 
 
 const id = [ 42, 57, 63, 98]
-const FirstName = ["Bruce", "Clark", "Tony", "Peter"]
+const firstname = ["Bruce", "Clark", "Tony", "Peter"]
 const occupation = ["DarkKnight","Man of Steel", "IronMan", "Friendly Neighborhood"]
 const age = [39, 33, 38, 27]
 
 console.log("ID", "FirstName", "occupation", "Age")
 
 for (let i = 0; i < id.length; i++) {
-    console.log(id[i] + " " FirstName[i] + " " occupation[i] + " " age[i])
+    console.log(id[i] ,  firstname[i] ,  occupation[i] , age[i])
 }
+
+console.log("...............")
+//////////////////////////////////////
+
+// Part 2: Expanding Functionality 
+
+const table = [id ,firstname, occupation, age]
+console.log(table)
+
